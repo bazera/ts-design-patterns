3795cb7efe6b1019c92bad748f5245763767a375
bazera <bazera.g@gmail.com>
2023-05-18 14:49:27 +0400

    initial

---
+++ index.ts
@@ -0,0 +1,63 @@
+interface FlyBehavior {
+  fly: () => void;
+}
+
+interface QuackBehavior {
+  quack: () => void;
+}
+
+class FlyWithWings implements FlyBehavior {
+  fly() {
+    console.log('flying with wings');
+  }
+}
+
+class FlyWithTail implements FlyBehavior {
+  fly() {
+    console.log('flying with tail');
+  }
+}
+
+class BasicQuack implements QuackBehavior {
+  quack() {
+    console.log('basic quacking');
+  }
+}
+
+class LoudQuack implements QuackBehavior {
+  quack() {
+    console.log('loud quacking');
+  }
+}
+
+abstract class Duck {
+  flyBehavior: FlyBehavior | undefined;
+  quackBehavior: QuackBehavior | undefined;
+
+  constructor() {}
+
+  performQuack() {
+    this.quackBehavior?.quack();
+  }
+
+  performFly() {
+    this.flyBehavior?.fly();
+  }
+
+  swim() {}
+
+  display() {}
+}
+
+class MallardDuck extends Duck {
+  constructor() {
+    super();
+    this.flyBehavior = new FlyWithTail();
+    this.quackBehavior = new LoudQuack();
+  }
+}
+
+const mallardDuck = new MallardDuck();
+mallardDuck.performFly();
+mallardDuck.flyBehavior = new FlyWithWings();
+mallardDuck.performFly();
